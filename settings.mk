0787ed5f468e12c88c5b4d9fdd3b4dadd5123b98
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2020-03-30 21:04:17 +0200

    minor

--- settings.mk
+++ settings.mk
@@ -1,5 +1,4 @@
 MXE_TARGETS := i686-w64-mingw32.static
-MXE_PLUGIN_DIRS := plugins/gcc7
 LOCAL_PKG_LIST := cc cmake qtbase qttools
 .DEFAULT_GOAL := local-pkg-list
 local-pkg-list: $(LOCAL_PKG_LIST)
